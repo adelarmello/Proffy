52c2416c69e4457b49f58c556f5d831b00e209b9
Adelar <adelarmello8@gmail.com>
2020-08-03 17:40:09 -0300

    created a new page called TeacherForm

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
 import TeacherList from "./pages/TeacherList";
+import TeacherForm from "./pages/TeacherForm";
 
 const Routes = () => (
   <Router>
     <Route path="/" exact component={Landing} />
     <Route path="/study" component={TeacherList} />
+    <Route path="/give-class" component={TeacherForm} />
   </Router>
 );
 
