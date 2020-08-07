0967f1f0a32c7e4f8e8dfc843e4c358b54bddf79
Adelar <adelarmello8@gmail.com>
2020-08-07 15:47:23 -0300

    Inserting Navigation tabs to the Favorites and TeacherList pages

---
+++ mobile/src/pages/TeacherList/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./styles";
+
+function TeacherList() {
+  return <View style={styles.container} />;
+}
+
+export default TeacherList;
